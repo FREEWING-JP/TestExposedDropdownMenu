22f576c69c165405ed467d33e30e9701c366ed57
FREEWING-JP <freewing.jp@gmail.com>
2020-12-06 00:33:18 +0900

    Add ArrayAdapter

--- app/src/main/java/com/example/test/app/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/test/app/ui/main/MainFragment.kt
@@ -1,11 +1,13 @@
 package com.example.test.app.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.test.app.R
 
 class MainFragment : Fragment() {
@@ -18,7 +20,19 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+
+        // Exposed Dropdown Menus
+        // https://material.io/develop/android/components/menu
+        val COUNTRIES = arrayOf("Item 1", "Item 2", "Item 3", "Item 4")
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+                context!!, R.layout.dropdown_menu_popup_item, COUNTRIES)
+        val editTextFilledExposedDropdown: AutoCompleteTextView = view.findViewById(R.id.filled_exposed_dropdown)
+        editTextFilledExposedDropdown.setAdapter(adapter)
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
